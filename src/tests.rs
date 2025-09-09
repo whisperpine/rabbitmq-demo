d7f3b66168b3a88992e8298d6a016431324543e0
Yusong <yusonglai64@gmail.com>
2025-09-10 03:09:08 +0800

    test: add sync test case "test_sync_mqtt"

--- src/tests.rs
+++ src/tests.rs
@@ -36,3 +36,34 @@ async fn test_async_mqtt() {
         println!("Received = {notification:?}");
     }
 }
+
+#[test]
+fn test_sync_mqtt() {
+    use rumqttc::{Client, MqttOptions, QoS};
+    use std::thread;
+    use std::time::{Duration, Instant};
+
+    let mut mqttoptions = MqttOptions::new("rumqtt-sync", HOST_NAME, 1883);
+    mqttoptions.set_keep_alive(Duration::from_secs(1));
+
+    let (client, mut connection) = Client::new(mqttoptions, 10);
+    client.subscribe("hello/rumqtt", QoS::AtMostOnce).unwrap();
+
+    thread::spawn(move || {
+        for i in 0..3 {
+            client
+                .publish("hello/rumqtt", QoS::AtLeastOnce, false, vec![i; i as usize])
+                .unwrap();
+            thread::sleep(Duration::from_millis(100));
+        }
+    });
+
+    let instant = Instant::now();
+    for notification in connection.iter() {
+        let notification = notification.unwrap();
+        println!("Notification = {notification:?}");
+        if instant.elapsed().as_secs_f32() > 2. {
+            break;
+        }
+    }
+}
